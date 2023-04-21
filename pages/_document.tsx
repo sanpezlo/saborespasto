4e073b820d5cc7b2a961d64721ab8899e4137078
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-04-21 16:53:38 -0500

    refactor: clean proyect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+
+import Header from "@/components/header";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
