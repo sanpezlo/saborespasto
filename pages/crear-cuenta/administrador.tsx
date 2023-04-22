a870741cc25cb46c34eae09f84b02a12cc6341dc
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-04-22 15:25:28 -0500

    feat: add placeholder to create admin

--- pages/crear-cuenta/administrador.tsx
+++ pages/crear-cuenta/administrador.tsx
@@ -71,6 +71,7 @@ export default function CrearCuentaAdministrador() {
                       name="fullname"
                       type="text"
                       autoComplete="fullname"
+                      placeholder="Pedro Sanchez"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       value={form.name}
@@ -94,6 +95,7 @@ export default function CrearCuentaAdministrador() {
                       name="email"
                       type="email"
                       autoComplete="email"
+                      placeholder="pedrosanchez@example.com"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       value={form.email}
@@ -116,6 +118,7 @@ export default function CrearCuentaAdministrador() {
                       id="password"
                       name="password"
                       type="password"
+                      placeholder="********"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       value={form.password}
