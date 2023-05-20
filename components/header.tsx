e9a4d26c996765b535ba24878c5b89aaa11dfd74
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-05-20 11:42:10 -0500

    fix: update header links

--- components/header.tsx
+++ components/header.tsx
@@ -276,9 +276,22 @@ export default function Header() {
                       >
                         Pedidos
                       </Link>
+                      <Link
+                        href="/mi-cuenta"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        Mi cuenta
+                      </Link>
                     </>
                   ) : (
-                    <></>
+                    <>
+                      <Link
+                        href="/mi-cuenta"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        Mi cuenta
+                      </Link>
+                    </>
                   )
                 ) : (
                   <>
